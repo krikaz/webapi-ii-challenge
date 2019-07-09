12de621ccf3a6088c331048db934b846b9be04c5
krikaz <pascal.bou@protonmail.com>
2019-07-09 14:08:08 +0200

    created all GET methods

--- data/db-routes.js
+++ data/db-routes.js
@@ -1,2 +1,51 @@
 const express = require('express');
 const Posts = require('./data/db');
+const route = express.Router();
+
+route.get('/', async (req, res) => {
+	try {
+		const posts = await Posts.find(req.query);
+		res.status(200).json(posts);
+	} catch (error) {
+		res
+			.status(500)
+			.json({ error: 'The posts information could not be retrieved.' });
+	}
+});
+
+route.get('/:id', async (req, res) => {
+	try {
+		const post = await Hubs.findById(req.params.id);
+
+		if (post) {
+			res.status(200).json(post);
+		} else {
+			res
+				.status(404)
+				.json({ message: 'The post with the specified ID does not exist.' });
+		}
+	} catch (error) {
+		res
+			.status(500)
+			.json({ error: 'The post information could not be retrieved.' });
+	}
+});
+
+route.get('/:id/comments', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const post = await Posts.findById(id);
+		if (post) {
+			const post = await Posts.findPostComments(id);
+			res.status(200).json(post);
+		} else {
+			res
+				.status(404)
+				.json({ message: 'The post with the specified ID does not exist.' });
+		}
+	} catch (error) {
+		res
+			.status(500)
+			.json({ error: 'The comments information could not be retrieved.' });
+	}
+});
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const postsRoutes = require('./data/db-routes');
 const server = express();
 
 server.use(express.json());
+server.use('/api/posts', postsRoutes);
 
 server.listen(3000, () => {
 	console.log('listening on 3000');
